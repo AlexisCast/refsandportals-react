000cec292a6ac1ca2409e987e632c9a59e688c34
Alexis Castellanos <castellanos.alexis@hotmail.com>
2023-12-07 03:12:08 -0600

    managing values with refs

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,39 @@
+import { useState, useRef } from "react";
+
 const TimerChallenge = ({ title, targeTime }) => {
+	const timer = useRef();
+
+	const [timerStarted, setTimerStarted] = useState(false);
+	const [timerExpired, setTimerExpired] = useState(false);
+
+	const handleStart = () => {
+		timer.current = setTimeout(() => {
+			setTimerExpired(true);
+		}, targeTime * 1000);
+
+		setTimerStarted(true);
+	};
+
+	const handleStop = () => {
+		clearTimeout(timer.current);
+		// setTimerStarted(false);
+	};
+
 	return (
 		<section className="challenge">
 			<h2> {title}</h2>
+			{timerExpired && <p>You Lost!</p>}
 			<p className="challenge-time">
 				{targeTime} second{targeTime > 1 ? "s" : ""}
 			</p>
 			<p>
-				<button>Start Challange</button>
+				<button onClick={timerStarted ? handleStop : handleStart}>
+					{timerStarted ? "Stop" : "Start"} Challange
+				</button>
 			</p>
-			<p className="">Time is running... / Timer inactive</p>
+			<p className={timerStarted ? "active" : undefined}>
+				{timerStarted ? "Time is running..." : "Timer inactive"}
+			</p>
 		</section>
 	);
 };
